5f704007588a9545fd6dea50cb5e01a67885dee0
Admin <Admin@DESKTOP-NTKR54J>
2024-01-12 11:51:05 +0530

    bank

--- student/src/main/java/com/jbk/controller/LoginController.java
+++ student/src/main/java/com/jbk/controller/LoginController.java
@@ -4,7 +4,7 @@ package com.jbk.controller;
 public class LoginController {
 	public static void main(String[] args) {
 		System.out.println("demo");
-		System.out.println("student");
+		System.out.println("bank");
 
 	}
 }
